39e6af23942dca74e4f6d0e7030cfd1b9c277370
Nguyen Ha Vinh Khang <khangnhv22@uef.edu.vn>
2025-06-07 21:44:11 +0700

    sửa model Tutor + thêm model BSDetail

--- FinalProject/src/main/java/com/uef/model/Tutor.java
+++ FinalProject/src/main/java/com/uef/model/Tutor.java
@@ -4,16 +4,30 @@
  */
 package com.uef.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+
 /**
  *
  * @author qnhat
  */
 public class Tutor extends People {
+    
+    @NotBlank(message = "Không được để trống")
     private int Fee;
-    private int SubId;
+    
+    private Subject Subject;
+    
+    @NotBlank(message = "Không được để trống")
+    @Size(min = 1, max = 100)
     private String Education;
+    
+    @NotBlank(message = "Không được để trống")
+    @Size(min = 1, max = 100)
     private String Experience;
     
+    private List<Session> Session;
     
     public enum Rating{
         One,
@@ -22,4 +36,38 @@ public class Tutor extends People {
         Four,
         Five
     }
+
+    public int getFee() {
+        return Fee;
+    }
+
+    public Subject getSubject() {
+        return Subject;
+    }
+
+    public String getEducation() {
+        return Education;
+    }
+
+    public String getExperience() {
+        return Experience;
+    }
+
+    public void setFee(int Fee) {
+        this.Fee = Fee;
+    }
+
+    public void setSubId(Subject Subject) {
+        this.Subject = Subject;
+    }
+
+    public void setEducation(String Education) {
+        this.Education = Education;
+    }
+
+    public void setExperience(String Experience) {
+        this.Experience = Experience;
+    }
+    
+    
 }
